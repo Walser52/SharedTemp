a0676fdea10cfce899fb057aca6a5eb28af1b695
sheharyar.gik@gmail.com <sheharyar.gik@gmail.com>
2025-08-10 12:24:04 +0500

    m

---
+++ Naseeb/LGPS/_adjustpaths.py
@@ -0,0 +1,15 @@
+import sys
+#print("ADJUSTIN PATHS")
+#sys.path.insert(0, "/home/fes33/Documents/GIK - R&D/Personal - Papers and Reports/--Cloned/doped") 
+sys.path.insert(0, "/home/fes33/Documents/GIK - R&D/Personal - Papers and Reports/--Cloned/pymatgen-io-espresso")    
+#sys.path.insert(0, "/home/fes33/Documents/GIK - R&D/Personal - Papers and Reports/--Cloned/vise")    
+
+sys.path.insert(0, "/home/fes33/Documents/GIK - R&D/Personal - Papers and Reports/--Cloned/")    
+
+
+# sys.path.insert(0, '/home/fes33/Documents/GIK - R&D/Personal - Papers and Reports/--Cloned/pymatgen-analysis-defects')
+
+import os
+apikey = '4a3cEY6Q0G6DyR1BcQQ8UaDGK4JO3KFU'
+os.environ["MAPI_KEY"] = apikey
+
